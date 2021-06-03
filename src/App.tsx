861e298922195ad23001c6c64a2a49610f80cd49
Dmytro <20IvanovDmitriy02@gmail.com>
2021-06-03 15:45:08 +0300

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {interval, Subject} from "rxjs";
+import { takeUntil} from "rxjs/operators";
+import ButtonsComponent from "./Components/ButtonsComponent";
+import TimeComponent from "./Components/TimeComponent";
+
 
 function App() {
-  return (
+    const [time, setTime] = useState(0)
+    const [status, setStatus] = useState(0)
+    const [start, setStart] = useState(false)
+    const [smth,setSmth] = useState(false)
+
+    useEffect(() => {
+
+        const unsubscribe = new Subject();
+        interval(10)
+            .pipe(takeUntil(unsubscribe))
+            .subscribe(() => {
+                if (start) {
+                    setTime(val => val + 1);
+                }
+            });
+        return () => {
+            unsubscribe.next(time);
+            unsubscribe.complete();
+        };
+    }, [start]);
+
+    const wait =()=>{
+        const sub = interval(10)
+            .subscribe(v=>{
+                setSmth(true)
+
+        if(smth){
+            if(start){
+                handleStop()
+            }
+            sub.unsubscribe()
+        }})
+        setTimeout(()=>{
+
+            sub.unsubscribe()
+            setSmth(false)
+        },500)
+    }
+
+
+
+    const handleStart =():void=>{
+        setStatus(1);
+        setStart(prevState => !prevState)
+    }
+    const handleReset =()=>{
+        setTime(0)
+
+    }
+
+
+
+    const handleStop =()=>{
+    setStart(false);
+        setStatus(0);
+
+    }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TimeComponent time={time}/>
+    <ButtonsComponent
+        start={handleStart}
+        stop={handleStop}
+        resume={handleReset}
+        status={status}
+        wait={wait}
+    />
     </div>
   );
 }
